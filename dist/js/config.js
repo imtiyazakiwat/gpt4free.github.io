2ff39d00b8a823a0a94a9fa165e7fe9ab0d5cadc
hlohaus <983577+hlohaus@users.noreply.github.com>
2025-04-19 22:02:24 +0200

    Add config.js file

---
+++ dist/js/config.js
@@ -0,0 +1,8 @@
+window.oauthConfig = {
+    clientId: '762e4f6f-2af6-437c-ad93-944cc17f9d23',
+    scopes: ['inference-api']
+}
+window.checkUrls = [
+    "https://g4f.hopto.org",
+    window.location.origin,
+];
