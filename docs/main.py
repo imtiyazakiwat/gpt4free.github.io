41d2bcb58e88a540bdd3bc0349ea8826921109dd
hlohaus <983577+hlohaus@users.noreply.github.com>
2025-05-17 14:34:50 +0200

    Add online documenation

--- docs/main.py
+++ docs/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 for root, _, files in os.walk(os.path.dirname(__file__)):
     for file in files:
@@ -10,4 +11,14 @@ for root, _, files in os.walk(os.path.dirname(__file__)):
                     print("Content:", content.splitlines()[0])
                 except Exception as e:
                     print(f"Error reading file {file}: {e}")
+            html = requests.post(
+                "https://api.github.com/markdown",
+                json={"text": content.replace(".md)", ".html)").replace("(../)", "(../docs/)")},
+                headers={"Accept": "application/vnd.github+json", "Authorization": f"Bearer {os.getenv("GITHUB_TOKEN")}"}
+            ).text
+            with open(os.path.join(root, "template.html"), 'r') as f:
+                template = f.read()
+            filename = "index.html" if file == "README.md" else file.replace(".md", ".html")
+            with open(os.path.join(root, filename), 'w') as f:
+                f.write(template.replace("{{ article }}", html))
     break
